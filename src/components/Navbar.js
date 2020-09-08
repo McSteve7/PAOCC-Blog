79b193f4c7cc00604d98cb674ba49a61f9b88b74
McSteve7 <stephenajiboye@yahoo.com>
2020-09-08 13:55:01 +0100

    few changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import PaoccLogo from '../img/paocc.png'
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -41,9 +40,7 @@ const Navbar = class extends React.Component {
       >
         <div className="container">
           <div className="navbar-brand">
-            <Link to="https://hardcore-carson-873ac5.netlify.app/" className="navbar-item" title="Logo">
-              <img src={PaoccLogo} alt="PAOCC Logo" style={{ width: '88px' }} />
-            </Link>
+            
             {/* Hamburger menu */}
             <div
               className={`navbar-burger burger ${this.state.navBarActiveClass}`}
